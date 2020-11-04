791c319ab6afac0fef5ba0f6e15769b48dc4f78c
shashidhar0508 <shashidhar0508@gmail.com>
2020-11-04 18:50:53 +0530

    Committed all files by formatting the files

--- set2/standard-libraries/timeit-mod.py
+++ set2/standard-libraries/timeit-mod.py
@@ -7,5 +7,5 @@ print(list1)""", number=1))
 
 print("time taken to execute code 5 times : ",
       timeit.timeit("""list1=["apple","banana","cherry"] 
-list1.remove("banana") 
+list1.remove("banana")
 print(list1)""", number=10))
